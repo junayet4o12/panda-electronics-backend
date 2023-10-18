b62023fe61ce61183bd70c187037b2c550543858
junayet <muhammadjunayetmaruf@gmail.com>
2023-10-18 19:01:39 +0600

    apple product has stored in database and the are displayed

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const cors = require('cors');
 require('dotenv').config()
-const { MongoClient, ServerApiVersion , ObjectId} = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express()
 const port = process.env.PORT || 3000
 
@@ -13,51 +13,75 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@clu
 console.log(uri);
 
 const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  }
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
 });
 
 
 async function run() {
     try {
-  
-      await client.connect();
-  
-      const database = client.db("TechnologyShop");
-      const companycollection = database.collection("company");
-  
-      
-  
-      app.get('/company', async(req, res)=> {
-        const company =  companycollection.find();
-        const result = await company.toArray();
-        res.send(result);
-  
-      })
-      
-  
-      
-  
-      
-  
-  
-      await client.db("admin").command({ ping: 1 });
-      console.log("Pinged your deployment. You successfully connected to MongoDB!");
+
+        await client.connect();
+
+        const database = client.db("TechnologyShop");
+        const companycollection = database.collection("company");
+        const applecollection = database.collection("apple");
+
+
+
+        app.get('/company', async (req, res) => {
+            const company = companycollection.find();
+            const result = await company.toArray();
+            res.send(result);
+
+        })
+        app.get('/company/:name', async (req, res) => {
+            const name = req.params.name;
+            const cursor = { name: (name) };
+            const company = await companycollection.findOne(cursor);
+            res.send(company);
+        })
+        app.post('/apple', async(req, res)=> {
+            const product = req.body;
+            const result = await applecollection.insertOne(product);
+            res.send(result);
+        })
+        app.get('/apple', async (req, res) => {
+            const appleproduct = applecollection.find();
+            const result = await appleproduct.toArray();
+            res.send(result);
+
+        })
+        app.get('/apple/:name', async (req, res) => {
+            const name = req.params.name;
+            const cursor = { name: (name) };
+            const product = await companycollection.findOne(cursor);
+            res.send(product);
+        })
+
+
+
+
+
+
+
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
     } finally {
-  
-  
+
+
     }
-  }
-  run().catch(console.dir);
-  
-  
-  app.get('/', (req, res) => {
+}
+run().catch(console.dir);
+
+
+app.get('/', (req, res) => {
     res.send('Hello World!')
-  })
-  
-  app.listen(port, () => {
+})
+
+app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
-  })
+})
